cd6a2f0470234c28cdf70a2d18e453e3ebeedcc7
Guillermo Indalecio Fernandez <guillermobox@gmail.com>
2015-03-17 12:35:14 +0100

    Add set and indent all file

--- utils.c
+++ utils.c
@@ -3,40 +3,50 @@
 #include <math.h>
 #include "utils.h"
 
+/*
+ * List structure
+ *
+ * Single linked list structure
+ */
 struct st_list {
-	int * value;
+	int *value;
 	int size;
 };
 
-typedef struct st_list * List;
+typedef struct st_list *list;
 
-List newList(void){
-	List l;
+list list_new(void)
+{
+	list l;
 	l = malloc(sizeof(struct st_list));
 	l->value = NULL;
 	l->size = 0;
 	return l;
 }
 
-void delList(List list){
-	if(list!=NULL){
-		if(list->value!=NULL)
+void list_del(list list)
+{
+	if (list != NULL) {
+		if (list->value != NULL)
 			free(list->value);
 		free(list);
 	}
 }
 
-int popList(List list, int index){
+int list_pop(list list, int index)
+{
 	int value, i, j;
-	if( index >= list->size ){
+	if (index >= list->size) {
 		printf("WOP! Accesing element %d of a %d list\n", index, list->size);
 		exit(1);
 	}
 	value = list->value[index];
-	i=0; j=0;
-	if(index != list->size-1){
-		for(i=0; i<list->size; i++,j++){
-			if(j == index) j = j+1;
+	i = 0;
+	j = 0;
+	if (index != list->size - 1) {
+		for (i = 0; i < list->size; i++, j++) {
+			if (j == index)
+				j = j + 1;
 			list->value[i] = list->value[j];
 		}
 	}
@@ -44,50 +54,39 @@ int popList(List list, int index){
 	return value;
 }
 
-int pushList(List list, int value){
+int list_push(list list, int value)
+{
 	list->size += 1;
 	list->value = realloc(list->value, list->size * sizeof(int));
-	list->value[list->size-1] = value;
+	list->value[list->size - 1] = value;
 }
 
-void showList(List list){
+void list_show(list list)
+{
 	int i;
-	for(i=0; i < list->size; i++)
+	for (i = 0; i < list->size; i++)
 		printf("%d ", list->value[i]);
 	printf("\n");
 }
 
-#define MAXPRIMES 200000
-
-int nprimes;
-int primelist[MAXPRIMES];
-
-int isprime(int n){
-	int top, i, prime;
-	top = (int)sqrt(n);
-	for(i=0; i<nprimes; i++){
-		prime = primelist[i];
-		if( prime > top ) break;
-		if( n%prime == 0 ) return 0;
-	}
-	if (prime < top)
-	return 1;
-}
-
-
-void darray_alloc(darray *a)
+/*
+ * Dynamic array
+ *
+ * This array grows as elements are added
+ */
+void darray_alloc(darray * a)
 {
 	a->alloc = 64;
 	a->used = 0;
 	a->values = malloc(a->alloc * sizeof(int));
 };
 
-void darray_free(darray *a)
+void darray_free(darray * a)
 {
 	free(a->values);
 };
 
-void darray_add(darray *a, int val)
+void darray_add(darray * a, int val)
 {
 	if (a->used == a->alloc) {
 		a->alloc *= 2;
@@ -96,57 +95,18 @@ void darray_add(darray *a, int val)
 	a->values[a->used++] = val;
 };
 
-darray primes;
-
-void init_primes()
-{
-	darray_alloc(&primes);
-	darray_add(&primes, 2);
-	darray_add(&primes, 3);
-};
-
-int is_prime(int x)
-{
-	int i, top, prime, lastprime;
-	top = (int) sqrt(x);
-
-	lastprime = primes.values[primes.used-1];
-	if (x <= lastprime) {
-		for (i = 0; i < primes.used; i++)
-			if (primes.values[i] == x) return 1;
-		return 0;
-	};
-
-	for (i = 0; i < primes.used; i++) {
-		prime = primes.values[i];
-		if (prime > top) return 1;
-		if (x % prime == 0) return 0;
-	};
-
-	lastprime = prime;
-	while (lastprime < top) {
-		lastprime += 2;
-		if (is_prime(lastprime)) {
-			darray_add(&primes, lastprime);
-		}
-	};
-
-	for (; i < primes.used; i++) {
-		prime = primes.values[i];
-		if (prime > top) return 1;
-		if (x % prime == 0) return 0;
-	};
-
-	return 1;
-};
-
+/*
+ * Binary tree
+ *
+ * Binary tree implementation
+ */
 struct st_btree_node {
-	struct st_btree_node * left;
-	struct st_btree_node * right;
+	struct st_btree_node *left;
+	struct st_btree_node *right;
 	int value;
 };
 
-typedef struct st_btree_node * btree;
+typedef struct st_btree_node *btree;
 
 btree btree_new_node(int val)
 {
@@ -209,9 +169,11 @@ int btree_check(btree root, int val)
 
 void btree_show(btree root, int indent)
 {
-	if (root == NULL) return;
+	if (root == NULL)
+		return;
 	int i = indent;
-	while (i--) putchar(' ');
+	while (i--)
+		putchar(' ');
 	printf("%d\n", root->value);
 	btree_show(root->left, indent + 2);
 	btree_show(root->right, indent + 2);
@@ -250,3 +212,13 @@ int btree_length(btree root)
 	return btree_length(root->left) + btree_length(root->right) + 1;
 };
 
+/*
+ * Set
+ *
+ * A set implemented using a binary tree.
+ */
+typedef btree set;
+
+#define set_add btree_unique_add
+#define set_new btree_new_node
+#define set_show btree_show
